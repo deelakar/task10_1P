796df21abeae59f2a67b0c9923bfeee92c31f421
deelakr <deelaka98@gmail.com>
2024-05-27 21:26:50 +0530

    TaskPrioritization adjusted

--- src/main/java/onTrack/TaskPrioritization.java
+++ src/main/java/onTrack/TaskPrioritization.java
@@ -1,33 +1,46 @@
 package onTrack;
 
-
 import onTrack.models.Task;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskPrioritization {
     private List<Task> tasks = new ArrayList<>();
+    private List<Integer> priorities = new ArrayList<>();
 
     public void addTask(Task task) {
         tasks.add(task);
+        priorities.add(tasks.size() - 1);
     }
 
     public void prioritizeTask(Task task, int priority) {
-        tasks.remove(task);
-        tasks.add(priority, task);
+        int index = tasks.indexOf(task);
+        if (index != -1) {
+            priorities.remove(Integer.valueOf(index));
+            if (priority >= priorities.size()) {
+                priorities.add(index);
+            } else {
+                priorities.add(priority, index);
+            }
+        }
+    }
+
+    public List<Task> getTasksSortedByPriority() {
+        return priorities.stream()
+                .map(tasks::get)
+                .collect(Collectors.toList());
     }
 
     public List<Task> getTasks() {
         return tasks;
     }
 
-    public List<Task> getTasksSortedByPriority() {
-        List<Task> sortedTasks = new ArrayList<>(tasks);
-        sortedTasks.sort((task1, task2) -> Integer.compare(tasks.indexOf(task1), tasks.indexOf(task2)));
-        return sortedTasks;
-    }
-
     public void sortTasksByPriority() {
-        tasks.sort((task1, task2) -> Integer.compare(tasks.indexOf(task1), tasks.indexOf(task2)));
+        tasks = getTasksSortedByPriority();
+        priorities.clear();
+        for (int i = 0; i < tasks.size(); i++) {
+            priorities.add(i);
+        }
     }
 }
